daefd62a5838e21dd9f3fd5a126da8bbc39f09cc
Fernando Lins <fernandolins088@gmail.com>
2021-12-16 22:56:43 -0300

    Adiciona import de categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import createCategoryController from '../modules/cars/useCases/createCategory';
+import importCategoriesController from '../modules/cars/useCases/importCategories';
 import listCategoriesController from '../modules/cars/useCases/listCategories';
 
 const categoriesRouter = Router();
+const upload = multer({
+  dest: './temp',
+});
 
 categoriesRouter.post('/', (req, res) => createCategoryController.handle(req, res));
 
 categoriesRouter.get('/', (req, res) => listCategoriesController.handle(req, res));
+
+categoriesRouter.post('/import', upload.single('file'), (req, res) => importCategoriesController.handle(req, res));
 
 export default categoriesRouter;
