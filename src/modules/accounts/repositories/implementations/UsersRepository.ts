940bf2f7a7ba7e93814900c47ca1ea0bca8cc399
Fernando Lins <fernandolins088@gmail.com>
2022-04-01 20:28:01 -0300

    Adiciona upload do avatar de usuário

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -12,16 +12,20 @@ class UsersRepository implements IUsersRepository {
   }
 
   async create({
+    id,
     name,
     email,
     password,
     driver_license,
+    avatar,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
+      id,
       name,
       email,
       password,
       driver_license,
+      avatar,
     });
 
     await this.repository.save(user);
@@ -31,6 +35,11 @@ class UsersRepository implements IUsersRepository {
     const user = await this.repository.findOne({
       email,
     });
+    return user;
+  }
+
+  async findById(id: string) {
+    const user = await this.repository.findOne(id);
     return user;
   }
 }
