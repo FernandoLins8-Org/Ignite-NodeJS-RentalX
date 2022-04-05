d7def8e58b041668faad8336ecc28ad8e9787503
Fernando Lins <fernandolins088@gmail.com>
2022-04-05 19:30:59 -0300

    Adiciona rota para criar carros

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,90 @@
+import CarsRepositoryInMemory from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import AppError from '@shared/errors/AppError';
+
+import CreateCarUseCase from './CreateCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it('should be able to create a new car', async () => {
+    const carInfo = {
+      name: 'Test car',
+      description: 'Description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Test Brand',
+      category_id: 'category',
+    };
+
+    await createCarUseCase.execute({
+      name: carInfo.name,
+      description: carInfo.description,
+      daily_rate: carInfo.daily_rate,
+      license_plate: carInfo.license_plate,
+      fine_amount: carInfo.fine_amount,
+      brand: carInfo.brand,
+      category_id: carInfo.category_id,
+    });
+
+    const car = await carsRepositoryInMemory.findByLicensePlate(carInfo.license_plate);
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('should not be able to create a car with existing license plate', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Test car',
+        description: 'Description',
+        daily_rate: 100,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Test Brand',
+        category_id: 'category',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Test car 2',
+        description: 'Description 2',
+        daily_rate: 200,
+        license_plate: 'ABC-1234',
+        fine_amount: 70,
+        brand: 'Test Brand',
+        category_id: 'category',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should create a new car as available by default', async () => {
+    const carInfo = {
+      name: 'Test car',
+      description: 'Description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Test Brand',
+      category_id: 'category',
+    };
+
+    await createCarUseCase.execute({
+      name: carInfo.name,
+      description: carInfo.description,
+      daily_rate: carInfo.daily_rate,
+      license_plate: carInfo.license_plate,
+      fine_amount: carInfo.fine_amount,
+      brand: carInfo.brand,
+      category_id: carInfo.category_id,
+    });
+
+    const registeredCar = await carsRepositoryInMemory.findByLicensePlate(carInfo.license_plate);
+
+    expect(registeredCar.available).toBe(true);
+  });
+});
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import authenticateRoutes from './authenticate.routes';
+import carsRoutes from './cars.routes';
 import categoriesRouter from './categories.routes';
 import specificationsRouter from './specifications.routes';
 import usersRouter from './users.routes';
@@ -11,5 +12,6 @@ router.use('/categories', categoriesRouter);
 router.use('/specifications', specificationsRouter);
 router.use('/users', usersRouter);
 router.use(authenticateRoutes);
+router.use('/cars', carsRoutes);
 
 export default router;
