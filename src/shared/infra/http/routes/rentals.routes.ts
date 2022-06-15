4b9bef37659e787606bc1e68be5a7ac35fbf7216
Fernando Lins <fernandolins088@gmail.com>
2022-06-15 03:12:20 -0300

    Adiciona listagem de alugueis por usuário

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import CreateRentalsController from '@modules/rentals/useCases/createRentals/CreateRentalsController';
+import ListRentalsByUserController from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 import ReturnRentalController from '@modules/rentals/useCases/returnRental/ReturnRentalController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -9,6 +10,7 @@ const rentalsRoutes = Router();
 
 const createRentalsController = new CreateRentalsController();
 const returnRentalController = new ReturnRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalsRoutes.post(
   '/',
@@ -20,6 +22,12 @@ rentalsRoutes.post(
   '/devolution/:id',
   ensureAuthenticated,
   returnRentalController.handle,
+);
+
+rentalsRoutes.get(
+  '/user',
+  ensureAuthenticated,
+  listRentalsByUserController.handle,
 );
 
 export default rentalsRoutes;
