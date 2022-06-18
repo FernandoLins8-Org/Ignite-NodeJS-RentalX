4805a7ab19ec7c5fe51cab155bad5962ef239081
Fernando Lins <fernandolins088@gmail.com>
2022-06-18 01:53:18 -0300

    Corrige nome do arquivo SendForgotPasswordMailController.ts

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import ResetPasswordController from '@modules/accounts/useCases/resetPassword/ResetPasswordController';
-import SendForgotPasswordMailController from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCaseController';
+import SendForgotPasswordMailController from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 
 const passwordRoutes = Router();
 
